dc9747b6cc440a65a318b977a24ea00bd46a4045
Aryan Baburajan <aryanbaburajan2007@gmail.com>
2025-05-11 16:44:27 +0530

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,168 @@
-import Image from "next/image";
+"use client";
+
+import * as React from "react";
+import { format } from "date-fns";
+import { Calendar as CalendarIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { processScore, Result } from "@/lib/processing";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [date, setDate] = React.useState<Date | undefined>(
+    new Date(2025, 3, 23)
+  );
+  const [file, setFile] = React.useState<File | null>();
+  const [result, setResult] = React.useState<Result>();
+  const [time, setTime] = React.useState("F");
+  const [isCalendarOpen, setIsCalendarOpen] = React.useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  return (
+    <main>
+      <div className="flex h-screen items-center font-[family-name:var(--font-geist-mono)]">
+        <div className="flex flex-col gap-6 w-xs mx-auto">
+          <h3 className="mb-2 text-2xl font-semibold">KEAM Mark Calculator</h3>
+          <div className="flex items-end">
+            <div className="w-3/5">
+              <Label className="mb-2 block">Choose your session.</Label>
+              <Popover open={isCalendarOpen} onOpenChange={setIsCalendarOpen}>
+                <PopoverTrigger asChild>
+                  <Button variant="outline" className="w-full justify-start">
+                    <CalendarIcon className="mr-2 h-4 w-4" />
+                    {date ? format(date, "PPP") : <span>pick a date</span>}
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0">
+                  <Calendar
+                    defaultMonth={new Date(2025, 3)}
+                    disabled={{
+                      before: new Date(2025, 3, 23),
+                      after: new Date(2025, 3, 29),
+                    }}
+                    disableNavigation
+                    mode="single"
+                    selected={date}
+                    onSelect={(e) => {
+                      setDate(e);
+                      setIsCalendarOpen(false);
+                    }}
+                    className="font-[family-name:var(--font-geist-mono)]"
+                    initialFocus
+                    required
+                  />
+                </PopoverContent>
+              </Popover>
+            </div>
+            <div className="w-2/5">
+              <Select value={time} onValueChange={setTime}>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Time" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="F">Morning</SelectItem>
+                  <SelectItem value="A">Afternoon</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+
+          <div>
+            <Label className="mb-2">
+              Upload your
+              <span className="font-bold">Candidate Response</span>
+              page.
+            </Label>
+            <Input
+              type="file"
+              onChange={(e) => {
+                setFile(e.target.files?.[0]);
+              }}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+          </div>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant="link" className="underline">
+                How to get the Candidate Response page?
+              </Button>
+            </DialogTrigger>
+            <DialogContent className="font-[family-name:var(--font-geist-mono)]">
+              <DialogHeader>
+                <DialogTitle className="my-2">
+                  How to get these documents:
+                </DialogTitle>
+                <DialogDescription asChild>
+                  <div className="flex flex-col gap-5">
+                    <p>
+                      1. Go to the Candidate response page from{" "}
+                      <a
+                        href="https://www.cee.kerala.gov.in/keam2025/anskey"
+                        className="text-primary underline-offset-4 underline"
+                        target="_blank"
+                      >
+                        www.cee.kerala.gov.in/
+                      </a>{" "}
+                      and press <span className="font-bold">Ctrl + S</span>
+                    </p>
+                    <p>
+                      2. Upload the{" "}
+                      <span className="font-bold">CEE Kerala.html</span> file.
+                    </p>
+                  </div>
+                </DialogDescription>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
+          <Button
+            onClick={async () =>
+              setResult(await processScore(date, file, time))
+            }
           >
-            Read our docs
-          </a>
+            Get Results
+          </Button>
+          {result != undefined && (
+            <div className="mt-10 grid grid-cols-2 gap-10">
+              <h2 className="text-3xl font-semibold m-auto">
+                {result.score}/600
+              </h2>
+              <div className="grid grid-rows-2 gap-10">
+                <Label>Correct: {result.correct}</Label>
+                <Label>Incorrect: {result.incorrect}</Label>
+              </div>
+            </div>
+          )}
+          <Label className="my-5 block">
+            Made by{" "}
+            <a
+              href="https://x.com/aryanbaburajan"
+              className="text-primary underline-offset-4 underline"
+              target="_blank"
+            >
+              @aryanbaburajan
+            </a>
+          </Label>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </main>
   );
 }
