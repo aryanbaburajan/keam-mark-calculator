e4f7935f34c3cec31c9cfdb4c7f57266f32f4465
Aryan Baburajan <aryanbaburajan2007@gmail.com>
2025-05-12 12:43:32 +0530

    feat: added images to metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,6 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "KEAM Mark Calculator",
-  description: "Created by @aryanbaburajan",
 };
 
 export default function RootLayout({
